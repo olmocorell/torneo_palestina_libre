70a51385b848d66ed164904ed615b4ad44bb8d42
olmocorell <olmocorell@gmail.com>
2023-12-11 19:00:32 +0100

    updating

--- src/inicio.py
+++ src/inicio.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 def inicio():
-    st.title("Equipos")
+    st.title("Bienvenide al torneo")
+    st.image('assets/cabecera.png', caption='Cabecera de la página', use_column_width=True)
     # Tu código para mostrar la información de los equipos
 
